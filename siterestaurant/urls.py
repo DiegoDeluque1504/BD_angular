f1b81204c85f9450b6bb91b44ddf70c6ab9db582
DiegoDeluque1504 <dadeluquecastillo@gmail.com>
2025-11-05 20:57:48 -0500

    feat: Implementar sistema completo de vistas y templates
    - Crear vistas para categories, dishes, orders y deliveries
    - Implementar URLs para todas las aplicaciones
    - Agregar templates HTML para mostrar datos
    - Configurar URLs principales en siterestaurant/urls.py
    - Estructura completa del frontend Django

--- siterestaurant/urls.py
+++ siterestaurant/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('myapps.categories.urls')),
+    path('', include('myapps.dishes.urls')),
+    path('', include('myapps.orders.urls')),
+    path('', include('myapps.deliveries.urls')),
 ]
